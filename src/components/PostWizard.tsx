a192ba82b732743b779eebc96dc678e5f986d62f
FruGosky <lukaszkomandzik@gmail.com>
2024-02-26 23:09:10 +0100

    feat(PostWizard): adding post implemented

--- src/components/PostWizard.tsx
+++ src/components/PostWizard.tsx
@@ -1,20 +1,43 @@
-import { SignOutButton, UserButton } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
 import type { UserResource } from "@clerk/types";
+import { useState } from "react";
+import { api } from "~/utils/api";
+import { Button } from "./ui/Button";
 
 type TPostWizardProps = {
 	user: UserResource;
 };
 
 export default function PostWizard(props: TPostWizardProps) {
+	const [message, setMessage] = useState("");
+
+	const ctx = api.useUtils();
+
+	const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+		onSuccess: () => {
+			setMessage("");
+			void ctx.posts.getAll.invalidate();
+		},
+	});
+
 	return (
 		<>
 			<UserButton />
 			<input
 				type="text"
 				className="grow bg-transparent outline-none"
-				placeholder="Type some message..."
+				placeholder="What is happening?!"
+				value={message}
+				onChange={(e) => setMessage(e.target.value)}
+				disabled={isPosting}
 			/>
-			<SignOutButton />
+			<Button
+				variant={"secondary"}
+				onClick={() => mutate({ content: message })}
+				disabled={isPosting}
+			>
+				Post
+			</Button>
 		</>
 	);
 }
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,7 +1,12 @@
 import { clerkClient } from "@clerk/nextjs";
 import type { User } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { z } from "zod";
+import {
+	createTRPCRouter,
+	privateProcedure,
+	publicProcedure,
+} from "~/server/api/trpc";
 
 const filterUserForClient = (user: User) => {
 	const { id, username, imageUrl } = user;
@@ -10,7 +15,10 @@ const filterUserForClient = (user: User) => {
 
 export const postsRouter = createTRPCRouter({
 	getAll: publicProcedure.query(async ({ ctx }) => {
-		const posts = await ctx.db.post.findMany({ take: 100 });
+		const posts = await ctx.db.post.findMany({
+			take: 100,
+			orderBy: { createdAt: "desc" },
+		});
 
 		const users = (
 			await clerkClient.users.getUserList({
@@ -34,4 +42,22 @@ export const postsRouter = createTRPCRouter({
 			};
 		});
 	}),
+	create: privateProcedure
+		.input(
+			z.object({
+				content: z.string().min(1).max(255),
+			}),
+		)
+		.mutation(async ({ ctx, input }) => {
+			const authorId = ctx.userId;
+
+			const post = await ctx.db.post.create({
+				data: {
+					authorId,
+					content: input.content,
+				},
+			});
+
+			return post;
+		}),
 });
