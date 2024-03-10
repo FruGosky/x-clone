b2f4c708e5ce0e166268cabb3c35d863c08d6060
FruGosky <lukaszkomandzik@gmail.com>
2024-03-10 14:13:33 +0100

    fix(UserProfileHeader): dynamic imageSize deleted

--- src/components/UserProfileHeader.tsx
+++ src/components/UserProfileHeader.tsx
@@ -3,23 +3,18 @@ import { type RouterOutputs } from "~/utils/api";
 
 type TUserProfileHeaderProps = {
 	userData: RouterOutputs["profile"]["getUserByUsername"];
-	imageSize?: number;
 };
 
 export default function UserProfileHeader(props: TUserProfileHeaderProps) {
-	const imageSize = props.imageSize ?? 128;
-
 	return (
 		<>
-			<div
-				className={`relative mb-[${imageSize / 2}px] h-36 bg-slate-600`}
-			>
+			<div className={`relative mb-[64px] h-36 bg-slate-600`}>
 				<Image
 					src={props.userData.imageUrl}
 					alt={`${props.userData.username}'s profile picture`}
-					height={imageSize}
-					width={imageSize}
-					className={`absolute bottom-0 left-0 -mb-[${imageSize / 2}px] ml-4 rounded-full border-4 border-black`}
+					height={128}
+					width={128}
+					className={`absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black`}
 				/>
 			</div>
 		</>
