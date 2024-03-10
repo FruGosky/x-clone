540d60ca10f1becac633fbdce8280b6a5d7957bc
FruGosky <lukaszkomandzik@gmail.com>
2024-03-10 11:58:17 +0100

    style(Head): Head moved from index to _app to be as default

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,19 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
 	return (
 		<ClerkProvider {...pageProps}>
+			<Head>
+				<title>X-Clone</title>
+				<meta
+					name="description"
+					content="Simple X-Clone page created for learning new stuff"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 			<Toaster position="bottom-right" />
 			<Component {...pageProps} />
 		</ClerkProvider>
